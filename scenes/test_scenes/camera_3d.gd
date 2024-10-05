6ebb864adcb7cd3a8323506feefa0782fdd2034a
homexhome <homexhome@proton.me>
2024-10-05 17:30:19 +0300

    Camera work

--- scenes/test_scenes/camera_3d.gd
+++ scenes/test_scenes/camera_3d.gd
@@ -1,7 +1,8 @@
 extends Camera3D
 @export var max_fov : float = 100.0
 @export var min_fov : float = 40
-
+@export var max_x : float = 5
+@export var min_x : float = -25
 @export var camera_speed : float = 10
 func _process(delta):
 	move_camera_with_mouse(delta)
@@ -17,7 +18,9 @@ func move_camera_with_mouse(delta):
 	#rotation.y = clamp(current_rotation.y, deg_to_rad(-50), deg_to_rad(-10))
 	
 	if Input.is_action_pressed("ui_right"):
+		if global_position.x >= max_x : return
 		translate(Vector3.RIGHT * delta * camera_speed)
 	if Input.is_action_pressed("ui_left"):
+		if global_position.x <= min_x : return
 		translate(Vector3.LEFT * delta * camera_speed)
 		
--- scripts/test_scripts/test_target.gd
+++ scripts/test_scripts/test_target.gd
@@ -15,28 +15,30 @@ func _physics_process(delta: float) -> void:
 	if (mouse_position.x < 0 or mouse_position.x > window_size.x or
 			mouse_position.y < 0 or mouse_position.y > window_size.y):
 				if mouse_position.x <= 0 :
+					if camera.global_position.x <= camera.min_x : return
 					camera.translate(Vector3.LEFT* delta * camera.camera_speed)
 				elif mouse_position.x >= window_size.x:
+					if camera.global_position.x >= camera.max_x : return
 					camera.translate(Vector3.RIGHT * delta * camera.camera_speed)
 				return
 
+	if is_instance_valid(ui.active_monster_button):
+		var space_state = get_world_3d().direct_space_state
+		var origin= camera.project_ray_origin(mouse_position)
+		var direction = camera.project_ray_normal(mouse_position)
+		var ray_length = camera.far
+		var end = origin + direction * ray_length
+		var query = PhysicsRayQueryParameters3D.create(origin, end, 1)
+		var result = space_state.intersect_ray(query)
+		mouse_position_3D = end
 
-	var space_state = get_world_3d().direct_space_state
-	var origin= camera.project_ray_origin(mouse_position)
-	var direction = camera.project_ray_normal(mouse_position)
-	var ray_length = camera.far
-	var end = origin + direction * ray_length
-	var query = PhysicsRayQueryParameters3D.create(origin, end, 1)
-	var result = space_state.intersect_ray(query)
-	mouse_position_3D = end
-
-	if not result.is_empty() and is_instance_valid(ui.active_monster_button):
-		mesh_to_take.show()
-		mouse_position_3D = result["position"]
-		#print("Расстояние : ", (mouse_position_3D - origin).length())
-		mesh_to_take.global_position = mouse_position_3D
-	else:
-		mesh_to_take.hide()
+		if not result.is_empty() and is_instance_valid(ui.active_monster_button):
+			mesh_to_take.show()
+			mouse_position_3D = result["position"]
+			#print("Расстояние : ", (mouse_position_3D - origin).length())
+			mesh_to_take.global_position = mouse_position_3D
+		else:
+			mesh_to_take.hide()
 		
 func _input(event: InputEvent) -> void:
 	if mesh_to_take.visible and event is InputEventMouseButton and event.is_pressed():
