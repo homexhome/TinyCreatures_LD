48f2c206ba41dd9839601e6d4b6916a6f6b99bce
homexhome <homexhome@proton.me>
2024-10-06 15:42:24 +0300

    Remove debug navigation ray

--- scripts/ui/start_game_button.gd
+++ scripts/ui/start_game_button.gd
@@ -1,9 +1,10 @@
 extends Button
 @export var scene_to_load : String
-
+@export var loading_screen : Control
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 	
 func _on_pressed():
+	loading_screen.show()
 	GameManager.change_game_state(GameManager.GAME_STATE.GAME)
 	get_tree().change_scene_to_file(scene_to_load) 
