35297d9fb1a737682619ce2a4c243022b142781c
HomeXHome <homexhome@proton.me>
2024-10-07 22:04:04 +0300

    ывфыв

--- scenes/ui/health_bar.gd
+++ scenes/ui/health_bar.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
 @export var label : Label3D
-
-func set_text(curr,max):
+@export var attack_color : Color
+@export var defend_color : Color
+func set_text(curr,max,character):
+	if character.is_in_group("Attackers"):
+		label.set_modulate(attack_color)
+	else:
+		label.set_modulate(defend_color)
 	label.text = str(curr, " / ", max)
