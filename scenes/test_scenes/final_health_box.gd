7c6bbd39154e93dd064510e4e96732ffbff61358
HomeXHome <homexhome@proton.me>
2024-10-07 22:44:31 +0300

    boss fix

--- scenes/test_scenes/final_health_box.gd
+++ scenes/test_scenes/final_health_box.gd
@@ -3,7 +3,7 @@ extends BaseCharacterComponent
 var max_health : float = 1000
 var current_health : float = 1000
 @export var audio : AudioStreamPlayer3D
-
+@export var health_text : Node3D
 func _ready() -> void:
 	GameManager.register_final_target(get_parent())
 
@@ -15,6 +15,8 @@ func take_damage(damage : float, _damage_owner : Character, _attack_type : Chara
 	print(damage)
 	var temp_health = current_health - damage
 	current_health = temp_health
+	health_text.show()
+	health_text.set_text(current_health,max_health,character)
 	if current_health <= 0:
 		Event.event_game_lost()
 		return
