ec905352b6dd146153a9cf8cc066931deac04289
homexhome <homexhome@proton.me>
2024-10-11 22:08:32 +0300

    New props

--- scenes/levels/loading_scene.gd
+++ scenes/levels/loading_scene.gd
@@ -4,10 +4,18 @@ extends Node3D
 @export var place_to_load : Node3D
 @export var scene_to_load : String
 
+var initialized : bool = false
 func _ready() -> void:
 	for scene in scenes_to_preload_array:
 		var _scene = scene.instantiate()
 		place_to_load.add_child(_scene)
-		await get_tree().physics_frame
-		_scene.queue_free()
-	get_tree().change_scene_to_file(scene_to_load) 
+		await get_tree().create_timer(0.1).timeout
+		#_scene.queue_free()
+	initialized = true
+	#get_tree().change_scene_to_file(scene_to_load) 
+
+func _process(delta: float) -> void:
+	if initialized:
+		initialized = false
+		get_tree().change_scene_to_file(scene_to_load) 
+		
